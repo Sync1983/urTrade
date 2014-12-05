64b27360e757de137c32c438c7cd4e17b2ee50fd
Sync1983 <1983sync@gmail.com>
2014-12-05 14:58:43 +0300

    change loaded prices ext list

--- protected/config/main.php
+++ protected/config/main.php
@@ -74,7 +74,7 @@ return array(
 		// this is used in contact page
 		'adminEmail'=>'sales@atc58.ru',
                     'providers_data'=>array(
-                    /*    'Ixora'=>array(
+                        'Ixora'=>array(
                             'login'=>'AVTOTEHS',
                             'pass'=>'6de6b09l',
                             'contract_id'=>'86951',
@@ -86,7 +86,7 @@ return array(
 						'Forum'=>array(
                             'login'=>'/prices/forum',
                             'pass' =>''
-                        ),*/
+                        ),
 						'Armtek'=>array(
                             'login'=>'/prices/armtek',
                             'pass' =>''
